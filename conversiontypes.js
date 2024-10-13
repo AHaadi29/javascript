73501212b27b41c35847d27e8a6e9ff52736ecb7
Abduulhaadi Bubere <abdulhaadibubere1@gmail.com>
2024-10-13 23:11:02 +0530

    Update conversiontypes.js

--- conversiontypes.js
+++ conversiontypes.js
@@ -1,3 +1,19 @@
-let score = 33
+let score = undefined
 
 console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+// "33" -> 33
+// "33abd" -> NaN
+// true=1 , false=0
+
+
+
+let isLoggedin = 1
+
+let booleanisLoggedin = Boolean(isLoggedin);
+console.log(booleanisLoggedin);
